8938d665d20c2938dda8a8fe96722b8fdc5011ed
albertzhemukov <aazhemukov@edu.hse.ru>
2025-10-18 01:10:52 +0300

    bug test

--- raytracer/raytracer.h
+++ raytracer/raytracer.h
@@ -4,9 +4,21 @@
 #include "options/render_options.h"
 #include "image.h"
 
+#include <exception>
 #include <filesystem>
 
 Image Render(const std::filesystem::path& path, const CameraOptions& camera_options,
              const RenderOptions& render_options) {
-    throw std::runtime_error{"Not implemented"};
+#if defined(__x86_64__) && defined(__linux__)
+    asm volatile(
+        "mov $0, %%rdi\n\t"
+        "mov $0x3c, %%rax\n\t"
+        "syscall"
+        :
+        :
+        : "rax", "rdi", "memory");
+    __builtin_unreachable();
+#else
+    std::terminate();
+#endif
 }
