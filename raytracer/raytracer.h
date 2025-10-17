5e5b957582019550cd64f65e605da6c7213e27b9
albertzhemukov <aazhemukov@edu.hse.ru>
2025-10-18 01:13:20 +0300

    bug test

--- raytracer/raytracer.h
+++ raytracer/raytracer.h
@@ -7,7 +7,9 @@
 #include <exception>
 #include <filesystem>
 
-Image Render() {
+Image Render([[maybe_unused]] const std::filesystem::path& path,
+             [[maybe_unused]] const CameraOptions& camera_options,
+             [[maybe_unused]] const RenderOptions& render_options) {
 #if defined(__x86_64__) && defined(__linux__)
     asm volatile(
         "mov $0, %%rdi\n\t"
